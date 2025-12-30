62027c4a9fdc52b8ab26a5a14fa095e04cd1e86d
Mateus <mateusmacedoeng@gmail.com>
2025-12-29 23:30:45 -0300

    add authenticate controller and creating private and public key, using jwt token with rsa 256 converted in base64

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,11 +9,16 @@ import { Env } from '@/env'
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
+      global: true,
       useFactory(config: ConfigService<Env, true>) {
-        const secret = config.get('JWT_SECRET', { infer: true })
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
 
+        // Pega um texto através de um enconding e converte ele para um buffer no node, e o buffer carrega dados em memória no node
         return {
-          secret,
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKeyKey: Buffer.from(publicKey, 'base64'),
         }
       },
     }),
